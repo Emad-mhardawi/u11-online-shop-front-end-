faa69d9b7b779a875f1d5cf6396b87fbd66a33dd
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-02-23 13:02:49 +0100

    add required components to build the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import NavBar from './Components/Navigation/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/Navigation/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+
+const NavBar = ()=>{
+    return(
+        <nav className='nav-bar'>
+sdsd
+        </nav>
+    )
+
+}
+
+export default NavBar;
+
